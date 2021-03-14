14d4616046afe7ce11200fa38c13bc1ced5e443a
Харченко Илья <kharchenko_ilia@mail.ru>
2021-03-14 11:48:21 +0700

    Fixed chessboard output(Output.c)

--- scr/libchessviz/Output.c
+++ scr/libchessviz/Output.c
@@ -5,7 +5,7 @@ void Output(char table[8][8])
 {
     int i, j;
     for (i = 0; i < 8; i++) {
-        printf("%d ", i + 1);
+        printf("%d ", 8 - i);
         for (j = 0; j < 8; j++)
             printf("%c ", table[i][j]);
         printf("\n");
