e2f2a0366381d657b616120f6dcacb94c36fb43d
Харченко Илья <kharchenko_ilia@mail.ru>
2021-03-27 15:02:11 +0700

    Added functions to header fules

--- scr/libchessviz/Move.h
+++ scr/libchessviz/Move.h
@@ -1,3 +1,6 @@
 #pragma once
 
 int Move(char s[],char table[8][8],int k);
+int first_pos(char s1[], char table[8][8]);
+int second_pos(char s1[], char table[8][8]);
+void mv(char s1[], char table[8][8]);
